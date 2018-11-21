a99d9d5a803e81348e3691c658e127e4079d617e
Alex75Git <45144806+Alex75Git@users.noreply.github.com>
2018-11-21 10:53:31 +0200

    Initial commit

---
+++ appTestGit/src/main/java/com/example/alexandr/gittest/ActivityGitTest.kt
@@ -0,0 +1,12 @@
+package com.example.alexandr.gittest
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ActivityGitTest : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_git_test)
+    }
+}
